a69046085be9841ff178e1062e3bd23a045b1662
Justin <twoplatinumchainz@gmail.com>
2019-12-14 22:01:20 -0800

    filled in a questions array object

---
+++ questions.js
@@ -0,0 +1,31 @@
+var questions = [
+    {
+        question: 'What city was Kanye West born in?',
+        choices: ['New York', 'Los Angeles', 'Chicago', 'Atlanta'],
+        answer: 'Atlanta'
+    },
+
+    {
+        question: 'What movie did Tupac Shakur star in?',
+        choices: ['Next Friday', 'Juice', 'Menace II Society', 'Boyz n the Hood'],
+        answer: 'Juice'
+    },
+
+    {
+        question: 'Which rapper became infamous for accidentally shooting himself?',
+        choices: ['Lil Wayne', '50 Cent', 'The Notorious B.I.G.', 'Snoop Dogg'],
+        answer: 'Lil Wayne'
+    },
+
+    {
+        question: 'Which rapper has won the most Grammy Awards?',
+        choices: ['Kendrick Lamar', 'Drake', 'Jay-Z', 'Eminem'],
+        answer: 'Jay-Z'
+    },
+
+    {
+        question: 'Which rapper has the largest net worth as of 2019?',
+        choices: ['Sean \'Diddy\' Combs', 'Dr. Dre', 'Kanye West', 'Jay-Z'],
+        answer: 'Jay-Z'
+    }
+];
