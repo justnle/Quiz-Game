d4b6ec114fffc57043e0f075bbe907a1bf3317c6
Justin <twoplatinumchainz@gmail.com>
2019-12-15 18:23:44 -0800

    basic quiz functionality

--- script.js
+++ script.js
@@ -1,59 +1,44 @@
 // variables
 var questionNum = 0;
 
-// timer function
-
-// high score submit function
-
-// quiz function
-
-$('#start-button').on('click', function() {
+// start the quiz by showing the question and hiding intro
+$('#start-button').on('click', function () {
     $('#quiz-intro').hide();
-    $('#question-choices').html('');
+    displayQuestion();
+});
 
+// displays the question by changing the title and choices
+function displayQuestion() {
     var questionTitle = questions[questionNum].question;
     var questionChoices = questions[questionNum].choices;
-    var questionAnswer = questions[questionNum].answer;
 
-    console.log(questionAnswer);
-    
-    $('#question-title').html('<h2>' + questionTitle + '</h2>');
+    $('#questions-choices').html('');
+    $('#question-title').html('<h3>' + questionTitle + '</h3>');
 
     for (var i = 0; i < questionChoices.length; i++) {
         var choice = questionChoices[i];
-
         var choiceList = $('<li></li>').text(choice);
         choiceList.attr('data-index', i);
         $('#question-choices').append(choiceList);
-
-        // $('#question-choices').append('<li>' + questionChoices[i] + '</li>');
-
     }
+}
 
+// allows user to choose answers
+$('#question-choices').click(function (event) {
+    $('#question-choices').html('');
+    var userChoice = event.target.dataset.index;
+    var questionAnswer = questions[questionNum].answer;
     questionNum++;
 
-    $('#question-choices').click(function(event) {
-        var userChoice = event.target.dataset.index;
-        console.log(userChoice);
-
-        if (userChoice === questionAnswer) {
-            alert('correct!');
-        } else {
-            alert('incorrect!');
-        }
-    });
-
-    // $('#question-choices').on('click', function() {
-
-
-    // });
-
-    while (questionNum === 5) {
-        questionNum = 0;
-        alert('You are all done!');
+    if (userChoice === questionAnswer) {
+        alert('correct!');
+        displayQuestion();
+    } else if (questionNum === 5) {
+        $('#question-title').html('<h3> you are done </h3>');
+        alert('you are done!');
+        return;
+    } else {
+        alert('incorrect!');
+        displayQuestion();
     }
 });
-
-// function userChoice() {
-
-// }
