700a690857a66e4d2406be3b7ff7eebbf3bd4c11
Justin <twoplatinumchainz@gmail.com>
2019-12-17 23:59:54 -0800

    moved js files to assets folder

---
+++ assets/js/questions.js
@@ -0,0 +1,31 @@
+var questions = [
+    {
+        question: 'What city was Kanye West born in?',
+        choices: ['New York', 'Los Angeles', 'Chicago', 'Atlanta'],
+        answer: '3'
+    },
+
+    {
+        question: 'What movie did Tupac Shakur star in?',
+        choices: ['Next Friday', 'Juice', 'Menace II Society', 'Boyz n the Hood'],
+        answer: '1'
+    },
+
+    {
+        question: 'Which rapper became infamous for accidentally shooting himself?',
+        choices: ['Lil Wayne', '50 Cent', 'The Notorious B.I.G.', 'Snoop Dogg'],
+        answer: '0'
+    },
+
+    {
+        question: 'Which rapper has won the most Grammy Awards?',
+        choices: ['Kendrick Lamar', 'Drake', 'Jay-Z', 'Eminem'],
+        answer: '2'
+    },
+
+    {
+        question: 'Which rapper has the largest net worth as of 2019?',
+        choices: ['Sean \'Diddy\' Combs', 'Dr. Dre', 'Kanye West', 'Jay-Z'],
+        answer: '3'
+    }
+];
