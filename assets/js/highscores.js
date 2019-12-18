83037388b8384f5eee9908ba90de981b99405598
Justin <twoplatinumchainz@gmail.com>
2019-12-18 00:52:41 -0800

    javascript for high score page

---
+++ assets/js/highscores.js
@@ -0,0 +1,8 @@
+// change to a real url later
+$('#back-button').on('click', function() {
+    location.href = "file:///Users/Justin/HW-4/index.html";
+});
+
+$('#clear-button').on('click', function() {
+    localStorage.clear();
+});
