81d3537830794066ae949cfd21ea469dd5dc3d46
Justin <twoplatinumchainz@gmail.com>
2019-12-18 23:10:15 -0800

    added a function to store/append scores

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -1,8 +1,20 @@
+$(document).ready(function() {
+    // for (var i = 0; i < )
+    var scoreInitials = localStorage.getItem('highScores');
+    var getScoreInitials = JSON.parse(scoreInitials);
+    $('#high-score-names').append(Object.keys(getScoreInitials)[0]);
+
+});
+
+// var existingHighScores = localStorage.getItem('high-scores');
+// console.log(existingHighScores);
+
 // change to a real url later
 $('#back-button').on('click', function() {
-    location.href = "file:///Users/Justin/HW-4/index.html";
+    location.href = 'index.html';
 });
 
 $('#clear-button').on('click', function() {
     localStorage.clear();
+    location.reload();
 });
