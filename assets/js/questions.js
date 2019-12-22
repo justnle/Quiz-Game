ae00e567254346494e892107d2a5989e1e1070d8
Justin <twoplatinumchainz@gmail.com>
2019-12-22 10:49:41 -0800

    added 5 more questions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -27,5 +27,36 @@ var questions = [
         question: 'Which rapper has the largest net worth as of 2019?',
         choices: ['Sean \'Diddy\' Combs', 'Dr. Dre', 'Kanye West', 'Jay-Z'],
         answer: '3'
+    },
+
+    {
+        question: 'What is the best selling Hip-Hop album as of 2019?',
+        choices: ['Marshall Mathers LP by Eminem', 'Life After Death by The Notorious B.I.G.', 'Speakerboxxx/The Love Below by OutKast', 'Licensed to Ill by The Beastie Boys'],
+        answer: '2'
+    },
+
+    {
+        question: 'How many members are there in the Wu-Tang Clan?',
+        choices: ['Eight', 'Nine', 'Ten', 'Eleven'],
+        answer: '2'
+    },
+    
+    {
+        question: 'What is NOT one of the five elements of Hip-Hop?',
+        choices: ['Breakdancing', 'Beat Production', 'Graffiti', 'Emceeing'],
+        answer: '1'
+
+    },
+
+    {
+        question: 'Who is the most sold Hip-Hop artist of all time, as of 2019?',
+        choices: ['Kanye West', 'Eminem', 'Drake', '2Pac'],
+        answer: '1'
+    },
+
+    {
+        question: 'Which of these pro athletes does/did NOT have a rap career?',
+        choices: ['Steve Largent', 'Clint Dempsey', 'Kobe Bryant', "Shaquille O'Neal"],
+        answer:'0'
     }
 ];
